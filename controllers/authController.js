47414c7f67ca371892f6549f6ad153bef39b54c0
Dishant106 <dishant.madariya@swiftrut.com>
2024-10-24 01:31:31 +0530

    Forget password Completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -75,3 +75,82 @@ module.exports.loginUser = async (req, res) => {
     }
 }
 
+module.exports.forgotPassword = async (req, res) => {
+    try {
+        if (req.body !== "") {
+            let checkmail = await User.findOne({ email: req.body.email });
+            if (checkmail) {
+                const Otp = Math.floor(100000 + Math.random() * 900000);
+                res.cookie('otp', Otp);
+                const transporter = nodemailer.createTransport({
+                    host: "smtp.gmail.com",
+                    port: 465,
+                    secure: true,
+                    auth: {
+                        user: process.env.EMAIL,
+                        pass: process.env.PASSWORD,
+                    },
+                });
+                const info = await transporter.sendMail({
+                    from: process.env.EMAIL,
+                    to: req.body.email,
+                    subject: "Forgot Password OTP ✔",
+                    text: `Hello ${checkmail.name}`,
+                    html: `<p>You're OTP is ${Otp}</p>`,
+                })
+                return res.status(200).json({ message: "OTP Sent Successfully 🎉", status: 1, data: Otp });
+            } else {
+                return res.status(400).json({ message: "Email is Incorrect", status: 0 });
+            }
+        } else {
+            return res.status(400).json({ message: "Data Not Found", status: 0 });
+        }
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).json({ message: "Something Wrong", status: 0 });
+    }
+}
+
+module.exports.verifyOtp = async (req, res) => {
+    try {
+        if (req.body !== "") {
+            if (req.body.otp == req.cookies.otp) {
+                return res.status(200).json({ message: "OTP Verified Successfully 🎉", status: 1 });
+            } else {
+                return res.status(400).json({ message: "OTP is Incorrect", status: 0 });
+            }
+        } else {
+            return res.status(400).json({ message: "Data Not Found", status: 0 });
+        }
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).json({ message: "Something Wrong", status: 0 });
+
+    }
+}
+
+module.exports.resetPassword = async (req, res) => {
+    try {
+        if (req.body !== "") {
+            let checkmail = await User.findOne({ email: req.body.email });
+            if (checkmail) {
+                if (req.body.password !== "" && req.body.password === req.body.confirmPassword) {
+                    let pass = await bcrypt.hash(req.body.password, 10);
+                    req.body.password = pass;
+                    await User.findByIdAndUpdate(checkmail._id, req.body);
+                    return res.status(200).json({ message: "Password Reset Successfully 🎉", status: 1 });
+                } else {
+                    return res.status(400).json({ message: "Password and Confirm Password is Not Matched", status: 0 });
+                }
+            } else {
+                return res.status(400).json({ message: "Email is Incorrect", status: 0 });
+            }
+        } else {
+            return res.status(400).json({ message: "Data Not Found", status: 0 });
+        }
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).json({ message: "Something Wrong", status: 0 });
+    }
+}
+
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, loginUser, forgotPassword, verifyOtp, resetPassword } = require('../controllers/authController');
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/forgot-password', forgotPassword);
+router.post('/verify-otp', verifyOtp);
+router.post('/reset-password', resetPassword);
 module.exports = router;
