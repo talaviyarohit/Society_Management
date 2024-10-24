5363dd44a24fc13ce5e0f2d1a04fb11ae28d0e2a
SrDhruv103 <dhruv.patel@swiftrut.com>
2024-10-24 22:19:01 +0530

    registration component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import ForgetPassword from './components/Login/Forget-password';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import OtpPage from './components/Login/Forget-otp';
 import Resetpassword from './components/Login/Resetpassword';
+import RegistrationForm from './components/Registration/Registration';
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,6 +21,7 @@ function App() {
         <Route path="/forget-password" element={<ForgetPassword />} />
         <Route path="/otppage" element={ <OtpPage/>} />
         <Route path="/resetpassword" element={  <Resetpassword/>} />
+        <Route path='/registration' element={<RegistrationForm/>}/>
       </Routes>
     </Router>
    
