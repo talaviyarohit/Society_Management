ffb1c70628184c139f14c0b4dbc7a7f4687ddf7b
srrohit110 <rohit.talaviya@swiftrut.com>
2024-10-28 18:02:52 +0530

    responsive final complated

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -112,10 +112,10 @@ export default function RegistrationForm() {
       <div className="flex flex-col lg:flex-row min-h-screen bg-gray-100">
         {/* Left side */}
         <div className="flex flex-col  justify-center px-4 sm:px-6 lg:flex-none lg:w-1/2 lg:px-20 xl:px-24 bg-gray-200">
-          <div className="mt-14">
+          <div className="">
             <h2 className="text-3xl font-bold text-gray-900"><span className='dash'>Dash</span>Stack</h2>
           </div>
-          <div className="flex justify-center mt-7">
+          <div className="flex justify-center mt-14">
             <img src={regImg} alt="Isometric illustration" className="max-w-md image1" />
           </div>
           <div className="text-center mt-12 lg:mt-20">
